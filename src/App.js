a0c3881361f80962f21168846058630b271ecd6e
Deimar <deimarparn14@gmail.com>
2022-11-17 16:32:55 +0200

    Created a swipe function on a like button click

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ import { FaTimes } from 'react-icons/fa'
 
 
 function App() {
+  let activeIndex = 0;
+  const groups = document.getElementsByClassName("card-group");
+
+  function handleHateClick() {
+    console.log("tere")
+  }
+
+  function handleLoveClick() {
+
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? 
+      activeIndex + 1 : 0;
+
+      const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+      const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+      currentGroup.dataset.status = "after"; 
+      nextGroup.dataset.status = "active";
+
+      activeIndex = nextIndex;
+    console.log(activeIndex);
+  }
+
   return (
   <div class="card-swiper">
     <div class="card-groups">
@@ -113,10 +135,10 @@ function App() {
       </div>
     </div>
     <div class="card-swiper-buttons">
-        <button id="hate-button">
+        <button id="hate-button" onClick={handleHateClick}>
           <FaTimes />
         </button>
-        <button id="love-button">
+        <button id="love-button" onClick={handleLoveClick}>
         <FaHeart />
         </button>
       </div>
